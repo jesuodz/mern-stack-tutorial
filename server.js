6b8945b806813f9391f8637f945b71154060bf27
Jesus Ordosgoitty <jesuodz@gmail.com>
2018-09-05 15:31:07 -0400

    version 1.0

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
-
+const path = require('path');
 const items = require('./routes/api/items');
 
 const app = express();
@@ -20,6 +20,16 @@ mongoose
 
 // Routing
 app.use('/api/items', items);
+
+// Serve static assets if in production
+if(process.env.NODE_ENV === 'production') {
+    // Set static folder
+    app.use(express.static('client/build'));
+
+    app.get('*', (request, response) => {
+        response.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+    });
+}
 
 const port = process.env.port || 5000;
 
